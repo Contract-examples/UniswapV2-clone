ad594ad98d2107dbddcaff29c4ac39b553c34df1
gmh5225 <2315157@qq.com>
2024-11-13 17:48:52 +0800

    building

--- src/periphery/test/WETH9.sol
+++ src/periphery/test/WETH9.sol
@@ -16,8 +16,8 @@
 pragma solidity ^0.8.28;
 
 contract WETH9 {
-    string public name = "Wrapped Ether";
-    string public symbol = "WETH";
+    string public name = "Mock Wrapped Ether";
+    string public symbol = "FWETH";
     uint8 public decimals = 18;
 
     event Approval(address indexed src, address indexed guy, uint256 wad);
