59d7cbe4c70c9cccb9f389c1b383220877339206
gmh5225 <2315157@qq.com>
2024-11-13 17:11:50 +0800

    fix compiler bugs

--- src/periphery/libraries/UniswapV2Library.sol
+++ src/periphery/libraries/UniswapV2Library.sol
@@ -25,7 +25,7 @@ library UniswapV2Library {
                             hex"ff",
                             factory,
                             keccak256(abi.encodePacked(token0, token1)),
-                            hex"96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f"
+                            hex"fad7b624020a0d6c9b7146c4b92bd08ab740db01de17bac2111932fc9b6193c2"
                         )
                     )
                 )
